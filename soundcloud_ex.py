8b30f6dd521676aba14a53102ce587f5b7e9d14d
aethos <ea2678@columbia.edu>
2015-01-11 16:04:29 +0900

    udating Souncloud example

--- soundcloud_ex.py
+++ soundcloud_ex.py
@@ -6,5 +6,11 @@ client = soundcloud.Client(client_id='13a223ddeda36f5e79f37680ae26e4ab')
 
 tracks = client.get('/tracks', q="Saturday Sessions", limit=3)
 
-for track in tracks:
-	print track.permalink_url
+
+def getURLS(trackList):
+    urls = ""
+    for track in trackList:
+        urls+= track.permalink_url
+    return trackURLS
+
+print getURLS(tracks)
